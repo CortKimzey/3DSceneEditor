37dff8222380051d25a679d01897d232835bf86e
CortKimzey <143350606+CortKimzey@users.noreply.github.com>
2025-04-05 09:40:51 -0500

    Add files via upload

--- 3DSceneEditor.c
+++ 3DSceneEditor.c
@@ -6,7 +6,7 @@ int main()
 {
     //system("for /r src/com %f in (*.java) do javac -d bin \"%f\"");
 
-    system("javac -d bin src/com/canvas/*.java src/com/frame/*.java src/*.java");
+    system("javac -d bin src/com/canvas/*.java src/com/frame/*.java src/*.java src/com/menu/*.java src/com/editor/*.java src/com/scene/*.java src/com/object/*.java src/com/use/*.java");
     system("echo Project Compiled");
 
     system("java -cp bin SceneEditor");
